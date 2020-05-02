6e4b2d2f0135fd2d25492e58569a38aae3d42108
Nathaniel Torrez <mr.realestatenate@gmail.com>
2020-05-02 14:13:45 -0700

    updated to the the style guide#

--- server/index.js
+++ server/index.js
@@ -1,14 +1,15 @@
-var express = require('express');
-var app = express();
-var bodyParser = require('body-parser');
-var port = 3003;
-var db = require('../database/index.js');
+const express = require('express');
+
+const app = express();
+const bodyParser = require('body-parser');
+
+const port = 3003;
+const db = require('../database/index.js');
 
 app.use(bodyParser.json());
 app.use(express.static('client/dist'));
 
-app.get("/reviews", (req, res) => {
-
+app.get('/reviews', (req, res) => {
   db.query('SELECT * FROM reviews;', (err, results) => {
     if (err) {
       res.status(404);
@@ -21,4 +22,4 @@ app.get("/reviews", (req, res) => {
 });
 
 
-app.listen(port, () => { console.log(`listening on port ${port}`);});
+app.listen(port, () => { console.log(`listening on port ${port}`); });
